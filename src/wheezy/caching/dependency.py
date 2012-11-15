e5dc68ee7f7cb30f8516b2c83afd465839e4edf0
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2012-11-15 19:23:04 +0200

    Added ability to have default namespace for CacheDependency.

--- src/wheezy/caching/dependency.py
+++ src/wheezy/caching/dependency.py
@@ -12,17 +12,19 @@ class CacheDependency(object):
         simplifing code necessary to manage dependencies in cache.
     """
 
-    __slots__ = ('cache', 'master_key', 'time')
+    __slots__ = ('cache', 'master_key', 'time', 'namespace')
 
-    def __init__(self, cache, master_key, time=0):
+    def __init__(self, cache, master_key, time=0, namespace=None):
         """
            *cache* - a cache instance to be used to track dependencies.
            *master_key* - a key used to track a number of issued dependencies.
            *time* - a time in seconds to keep dependent keys.
+           *namespace* - a default namespace.
         """
         self.cache = cache
         self.master_key = master_key
         self.time = time
+        self.namespace = namespace
 
     def next_key(self, namespace=None):
         """ Returns the next unique key for dependency.
@@ -36,7 +38,7 @@ class CacheDependency(object):
             'key2'
         """
         return self.master_key + str(self.cache.incr(
-            self.master_key, 1, namespace, 0))
+            self.master_key, 1, namespace or self.namespace, 0))
 
     def next_keys(self, n, namespace=None):
         """ Returns ``n`` number of dependency keys.
@@ -49,7 +51,8 @@ class CacheDependency(object):
             >>> d.next_keys(3)
             ['key2', 'key3', 'key4']
         """
-        last_id = self.cache.incr(self.master_key, n, namespace, 0)
+        last_id = self.cache.incr(self.master_key, n,
+                                  namespace or self.namespace, 0)
         return [self.master_key + str(i)
                 for i in xrange(last_id - n + 1, last_id + 1)]
 
@@ -64,6 +67,7 @@ class CacheDependency(object):
             >>> c.get('key1')
             'key-x'
         """
+        namespace = namespace or self.namespace
         return self.cache.add(self.next_key(namespace),
                               key, self.time, namespace)
 
@@ -89,6 +93,7 @@ class CacheDependency(object):
             >>> c.get('key4')
             'key-b'
         """
+        namespace = namespace or self.namespace
         mapping = dict(zip(self.next_keys(len(keys), namespace),
                            map(lambda k: key_prefix + k, keys)))
         return self.cache.add_multi(mapping, self.time, '', namespace)
@@ -119,6 +124,7 @@ class CacheDependency(object):
             >>> len(c.items)
             0
         """
+        namespace = namespace or self.namespace
         cache = self.cache
         n = cache.get(self.master_key, namespace)
         if n is None:
